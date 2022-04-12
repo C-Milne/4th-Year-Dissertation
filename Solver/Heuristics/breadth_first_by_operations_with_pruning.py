b2538dd2c3b6e611a24c1a9dc60352ffd193e912
Cael Milne <35670317+C-Milne@users.noreply.github.com>
2022-04-12 14:49:22 +0100

    Pruning can be inherited to heuristics

--- Solver/Heuristics/breadth_first_by_operations_with_pruning.py
+++ Solver/Heuristics/breadth_first_by_operations_with_pruning.py
@@ -1,28 +1,14 @@
-from Solver.Heuristics.Heuristic import Heuristic
+from Solver.Heuristics.pruning import Pruning
 
 """This is an extension to the breadth first search.
 Search can be initialised with multiple subtasks. After a model fully expands one subtask its current state is logged.
 If another model completes the same subtask and has the same current state, it is pruned."""
 
 
-class BreadthFirstOperationsPruning(Heuristic):
+class BreadthFirstOperationsPruning(Pruning):
     def __init__(self, domain, problem, solver, search_models):
         super().__init__(domain, problem, solver, search_models)
         self.low_target = True
-        self.seen_states = {}
 
     def ranking(self, model) -> float:
         return len(model.operations_taken)
-
-    def task_milestone(self, model) -> bool:
-        num_tasks_remaining = str(len(model.waiting_subtasks))
-        if num_tasks_remaining not in self.seen_states:
-            self.seen_states[num_tasks_remaining] = [self.solver.reproduce_state(model.current_state)]
-            return True
-        else:
-            reproduced_state = self.solver.reproduce_state(model.current_state)
-            if reproduced_state not in self.seen_states[num_tasks_remaining]:
-                self.seen_states[num_tasks_remaining].append(reproduced_state)
-                return True
-            else:
-                return False
--- Solver/Heuristics/distance_to_goal.py
+++ Solver/Heuristics/distance_to_goal.py
@@ -1,12 +1,11 @@
-from Solver.Heuristics.Heuristic import Heuristic
+from Solver.Heuristics.pruning import Pruning
 from Internal_Representation.problem_predicate import ProblemPredicate
 
 
-class PredicateDistanceToGoal(Heuristic):
+class PredicateDistanceToGoal(Pruning):
     def __init__(self, domain, problem, solver, search_models):
         super().__init__(domain, problem, solver, search_models)
         self.low_target = True
-        self.seen_states = {}
         self.goal_cons = []
 
     def ranking(self, model) -> float:
@@ -28,16 +27,3 @@ class PredicateDistanceToGoal(Heuristic):
                 else:
                     obs = [self.problem.get_object(x) for x in i[1:]]
                     self.goal_cons.append(ProblemPredicate(self.domain.get_predicate(i[0]), obs))
-
-    def task_milestone(self, model) -> bool:
-        num_tasks_remaining = str(len(model.waiting_subtasks))
-        if num_tasks_remaining not in self.seen_states:
-            self.seen_states[num_tasks_remaining] = [self.solver.reproduce_state(model.current_state)]
-            return True
-        else:
-            reproduced_state = self.solver.reproduce_state(model.current_state)
-            if reproduced_state not in self.seen_states[num_tasks_remaining]:
-                self.seen_states[num_tasks_remaining].append(reproduced_state)
-                return True
-            else:
-                return False
